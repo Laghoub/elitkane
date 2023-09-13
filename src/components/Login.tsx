1bf9a23b5b4eb42807c1e0afa8bab4fe99f23b0c
Laghoub <hn_laghoub@esi.dz>
2023-09-13 14:12:35 +0200

    chnge baghround

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -73,69 +73,81 @@ const Login = () => {
   }
 
   return (
-    <div className="container" style={{}}>
-      <div
-        className="row justify-content-center align-items-center login-container"
-        style={{ marginTop: "100px" }}
-      >
-        <div className="col-md-4 login-form-1">
-          {/* Logo */}
-          <center>
-            <img src={logo} alt="Logo" className="logo" />
-          </center>
-          <b>
-            <p style={{ color: "#007bff" }}>
-              Plateforme web développée par: LAGHOUB Nassim
-              <p>elitkane.website@gmail.com | Touts droits réservés</p>
-            </p>
-          </b>
-        </div>
+    <div>
+      <div className="container" style={{ background: "#022634" }}>
+        <div
+          className="row justify-content-center align-items-center login-container"
+          style={{ marginTop: "100px", padding: "40px" }}
+        >
+          <div className="col-md-4 login-form-1">
+            {/* Logo */}
+            <center>
+              <img src={logo} alt="Logo" className="logo" />
+            </center>
+            <b>
+              <p style={{ color: "white" }}>
+                Plateforme web développée par: LAGHOUB Nassim
+                <p>elitkane.website@gmail.com | Touts droits réservés</p>
+              </p>
+            </b>
+          </div>
 
-        <div className="col-md-4 login-form-2">
-          {/* Champs de saisie */}
-          <h2>Se connecter</h2>
-          <br />
-          {errorMessage && (
-            <div className="alert alert-danger">{errorMessage}</div>
-          )}
-          <div className="form-group">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Nom d'utilisateur"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+          <div className="col-md-4 login-form-2">
+            {/* Champs de saisie */}
+            <h2 style={{ color: "white" }}>Se connecter</h2>
+            <br />
+            {errorMessage && (
+              <div className="alert alert-danger">{errorMessage}</div>
+            )}
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Nom d'utilisateur"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <br />
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Mot de passe"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <br />
+            <div className="form-group">
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleLogin}
+              >
+                Se connecter
+              </button>
+            </div>
+            <br />
+            <p style={{ color: "white" }}>
+              Vous n'avez pas encore un compte ? inscrivez-vous.
+            </p>
+            <Link to="/teacherRegistration" style={{ color: "white" }}>
+              Inscription enseignant
+            </Link>
+            <br />
+            <Link to="/studentRegistration" style={{ color: "white" }}>
+              Inscription étudiant
+            </Link>
+            <br />
+            <Link to="/waiting" style={{ color: "white" }}>
+              Inscription parent
+            </Link>
           </div>
-          <br />
-          <div className="form-group">
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Mot de passe"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <br />
-          <div className="form-group">
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={handleLogin}
-            >
-              Se connecter
-            </button>
-          </div>
-          <br />
-          <p>Vous n'avez pas encore un compte ? inscrivez-vous.</p>
-          <Link to="/teacherRegistration">Inscription enseignant</Link>
-          <br />
-          <Link to="/studentRegistration">Inscription étudiant</Link>
-          <br />
-          <Link to="/waiting">Inscription parent</Link>
         </div>
       </div>
+      <br />
+      <br />
     </div>
   );
 };
